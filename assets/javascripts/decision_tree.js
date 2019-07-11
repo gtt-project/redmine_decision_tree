1cc8270bba87e45d82a28a2a8680a81ecefea326
Thibault <933kss@gmail.com>
2019-07-11 12:45:10 +0900

    scroll to modal after ajax request

--- assets/javascripts/decision_tree.js
+++ assets/javascripts/decision_tree.js
@@ -1,4 +1,7 @@
 $(document).on('click', 'button.decision-tree', function(e){
   $.get($(this).data('url'));
+  $(document).ajaxComplete(function(){
+    $('div.ui-dialog')[0].scrollIntoView()
+  })
   e.preventDefault();
 });
